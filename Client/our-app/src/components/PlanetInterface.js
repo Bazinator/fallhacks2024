cdf8f8a9a847b3059cda094a0e7e454b13271c92
Kendymann <pmjk.tr3@gmail.com>
2024-09-28 14:19:48 -0700

    planet interface

--- Client/our-app/src/components/PlanetInterface.js
+++ Client/our-app/src/components/PlanetInterface.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PlanetDisplay from './PlanetDisplay';
 import MessageTerminal from './MessageTerminal';
-import './PlanetInterface.css'; // For styling
+import './PlanetInterface.css';
 
 function PlanetInterface() {
+  const [selectedPlanet, setSelectedPlanet] = useState(null);
+
+  // For testing, you can set a default planet
+  // Remove or modify this in your actual implementation
+  React.useEffect(() => {
+    setSelectedPlanet({
+      name: 'Mars',
+      image: require('../assets/planets/mars.png'),
+    });
+  }, []);
+
   return (
     <div className="planet-interface">
-      <PlanetDisplay />
+      <PlanetDisplay selectedPlanet={selectedPlanet} />
       <MessageTerminal />
     </div>
   );
